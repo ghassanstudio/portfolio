186ef71fb5bb6ab251c4dd42a20ece322d1b81d1
Ghassan Studio <ghassan25vun@gmail.com>
2026-07-20 16:24:55 +0300

    Fix GitHub Pages site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://ghassanabdulkhaliq.github.io",
+  site: "https://ghassanstudio.github.io",
   base: "/portfolio",
   output: "static",
   build: {
